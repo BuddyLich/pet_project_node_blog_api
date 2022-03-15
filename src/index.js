6573d1b9c5d15293f148eccd2d4e25bf5d843476
David Zheng <57941361+BuddyLich@users.noreply.github.com>
2022-03-15 14:48:14 +1100

    remove unnecessary route function for index page

--- src/index.js
+++ src/index.js
@@ -8,15 +8,9 @@ app.use(express.json())
 mongoose.connect(process.env.MONGODB_URL)
 
 module.exports = app
-
 const port = process.env.PORT || 3000
-
-app.get('', (req, res) => {
-    res.status(200).send({
-        'message': 'Index'
-    })
-})
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
+
