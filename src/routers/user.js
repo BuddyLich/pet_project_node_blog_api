c225f08f61a0531520f0432c93e9fc7201da56e7
David Zheng <57941361+BuddyLich@users.noreply.github.com>
2022-03-15 17:42:00 +1100

    complete the get user route

--- src/routers/user.js
+++ src/routers/user.js
@@ -5,7 +5,7 @@ const User = require('../models/user')
 const router = new express.Router()
 
 // Incomplete
-router.post('/user', async (req, res) => {
+router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
@@ -14,8 +14,28 @@ router.post('/user', async (req, res) => {
         res.status(201).send({ user }) 
     } catch (e) {
         res.status(400).send(e)
-        console.log(e)
     }
+})
+
+router.get('/users/:username', async (req, res) => {
+    if (!req.params.username) {
+        return res.status(400).send({ error: "Username cannot be empty"})
+    }
+
+    const user = await User.findOne({ username: req.params.username })
+
+    if (!user) {
+        return res.status(404).send()
+    }
+
+    userPostNum = await Post.count({ user: user._id })
+
+    const userInfo =  {
+        username: user.username,
+        email: user.email,
+        createdPosts: userPostNum
+    }
+    return res.status(200).send(userInfo)
 })
 
 module.exports = router
