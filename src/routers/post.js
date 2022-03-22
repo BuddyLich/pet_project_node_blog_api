7a9b8c6b2924aa156af4ddcb0b2ea89b260c8316
David Zheng <57941361+BuddyLich@users.noreply.github.com>
2022-03-22 18:53:57 +1100

    Add new router for get post by id and update post by id

--- src/routers/post.js
+++ src/routers/post.js
@@ -17,4 +17,50 @@ router.post('/posts', auth, async (req, res) => {
     }
 })
 
+router.patch('/posts/:id', auth, async (req, res) => {
+    const _id = req.params.id
+
+    const updates = Object.keys(req.body)
+    const postFields = Object.keys(Post.schema.obj)
+
+    // filter out user field because the author of the post should not be changed
+    const allowUpdates = postFields.filter(field => field !== "user")
+    const isValidOperation = updates.every(update => allowUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: "Invalid updates." })
+    }
+    
+    try {
+        const post = await Post.findOne({ _id, user: req.user._id })
+
+        if (!post) {
+            res.status(404).send()
+        }
+        
+        updates.forEach((update) => {
+            post[update] = req.body[update]
+        })
+
+        await post.save()
+        res.send(post)
+
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+router.get('/posts/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            return res.status(404).send()
+        }
+
+        res.send(post)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
 module.exports = router
