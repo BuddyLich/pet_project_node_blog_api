1494dcb4bfd7893b0dcdedae940b4e1e6adeb5d8
David Zheng <57941361+BuddyLich@users.noreply.github.com>
2022-03-22 19:21:05 +1100

    change the response for user deletion. Now nothing will be sent after user deletion

--- src/routers/user.js
+++ src/routers/user.js
@@ -119,7 +119,7 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/user/me', auth, async (req, res) => {
     try {
         await req.user.remove()
-        res.send(req.user)
+        res.send()
     } catch (e) {
         res.send(500).send(e)
     }
