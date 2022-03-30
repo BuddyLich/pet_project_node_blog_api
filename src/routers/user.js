3c3d84feb3ae21595c68d4f2aaf53569c67c815b
David Zheng <57941361+BuddyLich@users.noreply.github.com>
2022-03-30 21:33:37 +1100

    Fix the typo for the user delete route

--- src/routers/user.js
+++ src/routers/user.js
@@ -102,7 +102,7 @@ router.patch('/users/me', auth, async (req, res) => {
     }
 })
 
-router.delete('/user/me', auth, async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove()
         res.send()
