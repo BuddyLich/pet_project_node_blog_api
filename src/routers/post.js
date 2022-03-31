eb631d0b9157be1c103da917aee4b226668e27d2
David Zheng <57941361+BuddyLich@users.noreply.github.com>
2022-03-31 12:35:24 +1100

    Remove the auth middleware for all get post router. Since by design all users should be able to get posts even when unauthenticated

--- src/routers/post.js
+++ src/routers/post.js
@@ -51,7 +51,7 @@ router.patch('/posts/:id', auth, async (req, res) => {
     }
 })
 
-router.get('/posts/:id', auth, async (req, res) => {
+router.get('/posts/:id', async (req, res) => {
     try {
         const post = await Post.findById(req.params.id).populate('user', 'username')
         if (!post) {
@@ -64,7 +64,7 @@ router.get('/posts/:id', auth, async (req, res) => {
     }
 })
 
-router.get('/posts', auth, async (req, res) => {
+router.get('/posts', async (req, res) => {
     const match = {}
     const sort = {}
 
